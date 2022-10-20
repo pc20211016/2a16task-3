62a63d4fb70d690a46a25bf579087007c1e8ffec
tuxedosam <115674630+pc20211016@users.noreply.github.com>
2022-10-20 09:41:44 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+input.onButtonPressed(Button.A, function () {
+    while (eee >= 0) {
+        basic.showNumber(eee)
+        eee += -1
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    eee = -1
+})
+let eee = 0
+eee = 9
+basic.forever(function () {
+	
+})
